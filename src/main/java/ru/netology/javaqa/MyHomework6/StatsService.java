2b49449c42918807fd112d9af798501b975920ad
Vladislav Andreev <vladireft96@mail.ru>
2024-11-30 23:36:14 +0500

    Change

--- src/main/java/ru/netology/javaqa/MyHomework6/StatsService.java
+++ src/main/java/ru/netology/javaqa/MyHomework6/StatsService.java
@@ -2,24 +2,24 @@ package ru.netology.javaqa.MyHomework6;
 
 public class StatsService {
 
-    public int amountOfSales(int[] sales) { //сумма продаж
+    public int amountOfSales(long[] sales) { //сумма продаж
         int amount = 0;
 
-        for (int i = 0; i < sales.length; i++) {
-            amount = amount + sales[i];
+        for (long sale : sales) {
+            amount = amount + (int) sale;
         }
 
         return amount;
     }
 
-    public int averageSalesAmount(int[] sales) { //средняя сумма продаж
+    public int averageSalesAmount(long[] sales) { //средняя сумма продаж
         int amount = amountOfSales(sales);
         int average = amount / sales.length;
 
         return average;
     }
 
-    public int maxAmountMonth(int[] sales) { //номер месяца с максимальными продажами
+    public int maxAmountMonth(long[] sales) { //номер месяца с максимальными продажами
         int maxMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
@@ -30,7 +30,7 @@ public class StatsService {
         return maxMonth + 1;
     }
 
-    public int minAmountMonth(int[] sales) { //номер месяца с минимальными продажами
+    public int minAmountMonth(long[] sales) { //номер месяца с минимальными продажами
         int minMonth = 0;
 
         for (int i = 0; i < sales.length; i++) {
@@ -41,7 +41,7 @@ public class StatsService {
         return minMonth + 1;
     }
 
-    public int monthSalesAboveAverage(int[] sales) { //количество месяцев с продажами выше среднего
+    public int monthSalesAboveAverage(long[] sales) { //количество месяцев с продажами выше среднего
         int average = averageSalesAmount(sales);
         int count = 0;
 
@@ -53,8 +53,8 @@ public class StatsService {
         return count;
     }
 
-    public int monthSalesBelowAverage(int[] sales) { //количество месяцев с продажами ниже среднего
-        int average = averageSalesAmount(sales);
+    public int monthSalesBelowAverage(long[] sales) { //количество месяцев с продажами ниже среднего
+        double average = averageSalesAmount(sales);
         int count = 0;
 
         for (int i = 0; i < sales.length; i++) {
--- src/test/java/ru/netology/javaqa/MyHomework6/StatsServiceTest.java
+++ src/test/java/ru/netology/javaqa/MyHomework6/StatsServiceTest.java
@@ -8,9 +8,9 @@ public class StatsServiceTest {
     public void shouldCalculateAmountOfSales() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
-        int actual = service.amountOfSales(sales);
+        long actual = service.amountOfSales(sales);
 
         Assertions.assertEquals(expected, actual);
     }
@@ -19,7 +19,7 @@ public class StatsServiceTest {
     public void shouldCalculateAverageSalesAmount() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 15;
         int actual = service.averageSalesAmount(sales);
 
@@ -30,7 +30,7 @@ public class StatsServiceTest {
     public void shouldCalculateMaxAmountMonth() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 8;
         int actual = service.maxAmountMonth(sales);
 
@@ -41,7 +41,7 @@ public class StatsServiceTest {
     public void shouldCalculateMinAmountMonth() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 9;
         int actual = service.minAmountMonth(sales);
 
@@ -52,7 +52,7 @@ public class StatsServiceTest {
     public void shouldCalculateMonthSalesAboveAverage() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
         int actual = service.monthSalesAboveAverage(sales);
 
@@ -63,7 +63,7 @@ public class StatsServiceTest {
     public void shouldCalculateMonthSalesBelowAverage() {
         StatsService service = new StatsService();
 
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
         int actual = service.monthSalesBelowAverage(sales);
 
